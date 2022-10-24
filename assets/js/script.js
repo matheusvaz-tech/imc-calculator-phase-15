31e662abc78ad88b3ff3f7cf72022bb598d68b38
Matheus Vaz <112774634+matheusvaz-dev@users.noreply.github.com>
2022-10-24 19:32:25 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,8 @@ const form = document.querySelector("form");
 const inputWeight = document.querySelector("#weight");
 const inputHeight = document.querySelector("#height");
 
+inputWeight.oninput = () => AlertError.close();
+inputHeight.oninput = () => AlertError.close();
 form.onsubmit = function (event) {
   event.preventDefault();
 
